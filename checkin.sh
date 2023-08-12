5bc66ca44e3fd793f71d8d024630766e08054ece
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-12 15:28:51 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -6,6 +6,10 @@ curl --http1.1 -X POST 'https://glados.rocks/api/user/checkin' \
   -H "cookie: ${COOKIE}" \
   -d '{"token": "glados.rocks"}'  | grep -Eo '"message":"[^"]*"'
 
+curl --http1.1 -X POST 'https://glados.rocks/api/user/checkin' \
+  -H 'content-type: application/json;charset=UTF-8' \
+  -H "cookie: ${COOKIE}" \
+  -d '{"token": "glados.one"}'  | grep -Eo '"message":"[^"]*"'
 
 echo '-----------------status-----------------'
 curl --http1.1 -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
