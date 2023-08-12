b7d9b5215c76d77c50699d2fcd0a6ca8896cb076
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-12 14:57:47 +0800

    Create checkin.sh

---
+++ checkin.sh
@@ -0,0 +1,14 @@
+curl 'https://glados.rocks/console/checkin' \
+  -H 'authority: glados.rocks' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: zh-CN,zh;q=0.9' \
+  -H 'cache-control: no-cache' \
+  -H "cookie:${COOKIE}" \
+  -H 'pragma: no-cache' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1' \
+  --compressed
