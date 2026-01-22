7108d3a7285acedc540343f2c3ec2e123d66f8d3
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2026-01-23 02:11:40 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,15 +1,14 @@
 echo '------------------sign------------------'
 ## curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.rocks/api/user/checkin' -s | grep -Eo '"message":"[^"]*"'
 
-curl --http1.1 -X POST 'https://glados.rocks/api/user/checkin' \
-  -H 'content-type: application/json;charset=UTF-8' \
-  -H "cookie:${COOKIE}" \
-  -d '{"token":"glados.rocks"}'  | grep -Eo '"message":"[^"]*"'
-
-curl --http1.1 -X POST 'https://glados.rocks/api/user/checkin' \
-  -H 'content-type: application/json;charset=UTF-8' \
-  -H "cookie:${COOKIE}" \
-  -d '{"token":"glados.one"}'  | grep -Eo '"message":"[^"]*"'
+curl -s -X POST "https://glados.cloud/api/user/checkin" \
+-H "cookie: ${COOKIE}" \
+-H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36" \
+-H "content-type: application/json;charset=UTF-8" \
+-H "origin: https://glados.cloud" \
+-H "referer: https://glados.cloud/console/checkin" \
+-d '{"token":"glados.cloud"}'  | grep -Eo '"message":"[^"]*"'
 
 echo '-----------------status-----------------'
-curl --http1.1 -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
+curl 'https://glados.cloud/api/user/status' \
+  -b "${COOKIE}" | grep -Eo '"leftDays":"[^"]*"' 
